a4c2bcd788f4c32078b1ff441cc2c278e72aee6d
aleesteele <aleesteele@gmail.com>
2021-02-02 17:33:31 +0000

    basic updates

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `979797`,
       marginBottom: `1.45rem`,
     }}
   >
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,13 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>This is going to turn into a cool website.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/element-list/">Go to element list</Link> <br />
+    <Link to="/timeline/">Go to timeline of element</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
