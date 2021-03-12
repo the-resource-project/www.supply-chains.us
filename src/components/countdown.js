90e80be9eea0c4b2a237efa2db66886d9833bab8
aleesteele <aleesteele@gmail.com>
2021-03-12 22:58:31 +0000

    new date

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -6,7 +6,7 @@ import styles from "../styles/layout.css"
 function Countdown() {
   const calculateTimeLeft = () => {
     let year = new Date().getFullYear();
-    const difference = +new Date(`${year}-03-13`) - +new Date();
+    const difference = +new Date(`${year}-03-15`) - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
@@ -14,7 +14,7 @@ function Countdown() {
         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
         hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
         minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
+        seconds:4Math.floor((difference / 1000) % 60),
       };
     }
 
